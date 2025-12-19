cb6f82e81f88889103453f15835b16706ec85609
hannnmstfa <burhanmusthofa1@gmail.com>
2025-12-19 11:53:29 +0700

    update setting git

--- resources/views/livewire/daftar-produk.blade.php
+++ resources/views/livewire/daftar-produk.blade.php
@@ -100,21 +100,21 @@
                         <div
                             class="absolute bottom-0 font-inter md:grid grid-cols-4 mb-3 md:mb-0 left-0 w-full justify-center items-center space-y-1 md:space-y-0 gap-[2px]">
                             <button
-                                class="col-span-3 peer flex justify-center items-center text-xs border border-yellow-600 hover:bg-yellow-600 text-yellow-600 hover:text-white font-bold py-2 px-4 shadow-md transition-colors duration-100">
+                                class="col-span-3 flex justify-center items-center text-xs border border-yellow-600 hover:bg-yellow-600 text-yellow-600 hover:text-white font-bold py-2 px-4 shadow-md transition-colors duration-100">
                                 <svg class="w-5 h-5 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
                                     height="24" fill="none" viewBox="0 0 24 24">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                         stroke-width="2"
                                         d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6" />
                                 </svg>
-                                <span class="ms-1 hidden peer-hover:block">Tambah Keranjang</span>
+                                <span class="ms-1">Tambah ke Keranjang</span>
                             </button>
-                            <div class=" peer col-span-1 hover:col-span-3">
-                                <button data-tooltip-target="tooltip-default" type="button"
+                            <div class="col-span-1">
+                                <button data-tooltip-target="tooltiipid" type="button"
                                     class="w-full text-xs bg-red-800 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded-md shadow-md transition-colors duration-300">
                                     Beli
                                 </button>
-                                <div id="tooltip-default" role="tooltip"
+                                <div id="tooltiipid" role="tooltip"
                                     class="absolute z-20 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-dark rounded-base shadow-xs opacity-0 tooltip">
                                     Tooltip content
                                     <div class="tooltip-arrow" data-popper-arrow></div>
