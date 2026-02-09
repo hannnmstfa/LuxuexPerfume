65a6e0248e6d734cd59f2ccfb95a953f5056bfc7
hannnmstfa <burhanmusthofa1@gmail.com>
2026-02-09 22:38:04 +0700

    fix form

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,10 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(config("app.env") == 'local') {
-            URL::forceScheme('http');
-        }else{
-            URL::forceScheme('https');
-        }
+        // if(config("app.env") == 'local') {
+        //     URL::forceScheme('http');
+        // }else{
+        //     URL::forceScheme('https');
+        // }
     }
 }
