cf619f323171779382b5875ce4fe6d29ec2ed6a0
hannnmstfa <burhanmusthofa1@gmail.com>
2026-04-27 19:12:35 +0700

    update konten chat

--- resources/views/components/chatbot.blade.php
+++ resources/views/components/chatbot.blade.php
@@ -18,29 +18,30 @@
             </div>
 
             <!-- BODY CHAT -->
-            <div class="flex flex-col h-[28rem]">
+            <div class="flex flex-col max-h-[70dvh]">
 
                 <!-- MESSAGES (overflow di sini) -->
                 <div id="chatBot-messages" class="flex-1 overflow-y-auto px-3 py-4 bg-slate-950 scroll-style"></div>
-                <div class="w-full flex flex-col gap-1 relative">
-                    <div class="flex justify-start items-center overflow-x-auto text-white text-xs m-2 scroll-style gap-2 pb-2">
-                        <button class="p-2 border rounded-xl hover:bg-gray-800 text-nowrap">Pesanan saya sampai mana</button>
-                        <button class="p-2 border rounded-xl hover:bg-gray-800 text-nowrap">Pesanan saya sampai mana</button>
-                        <button class="p-2 border rounded-xl hover:bg-gray-800 text-nowrap">Pesanan saya sampai mana</button>
-                        <button class="p-2 border rounded-xl hover:bg-gray-800 text-nowrap">Pesanan saya sampai mana</button>
+                <div class="w-full flex flex-col relative">
+                    <div
+                        class="flex justify-start items-center overflow-x-auto text-white text-xs m-2 scroll-style gap-2 px-1 pb-2">
+                        <button class="p-2 border rounded-xl hover:bg-gray-800 text-nowrap" data-value="hidup jokosiii" id="chat-001">Rekomendasi Produk</button>
+                        <button class="p-2 border rounded-xl hover:bg-gray-800 text-nowrap" data-value="hidup jokosiii" id="chat-002">Rekomendasi Produk</button>
+                        <button class="p-2 border rounded-xl hover:bg-gray-800 text-nowrap" data-value="hidup jokosiii" id="chat-003">Rekomendasi Produk</button>
+                        <button class="p-2 border rounded-xl hover:bg-gray-800 text-nowrap" data-value="hidup jokosiii" id="chat-004">Rekomendasi Produk</button>
                     </div>
                     <!-- INPUT -->
                     <form id="chatBot-form" class="border-t border-white/10 bg-slate-900 p-3">
                         <div class="flex items-center justify-between gap-2">
                             <input id="chatBot-input" type="text" placeholder="Tulis pesan..." autocomplete="off"
-                            class="h-11 w-full rounded-full border border-white/10 bg-slate-800 px-4 text-sm text-white placeholder:text-slate-400 focus:border-emerald-400 focus:outline-none">
+                                class="h-11 w-full rounded-full border border-white/10 bg-slate-800 px-4 text-sm text-white placeholder:text-slate-400 focus:border-emerald-400 focus:outline-none">
                             <button id="chatBot-send"
-                            class="h-11 w-max rounded-full bg-gold px-4 text-sm font-semibold text-black hover:opacity-85">
-                            Kirim
-                        </button>
-                    </div>
-                </form>
-            </div>
+                                class="h-11 w-max rounded-full bg-gold px-4 text-sm font-semibold text-black hover:opacity-85">
+                                Kirim
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
 
@@ -58,4 +59,26 @@
             </svg>
         </button>
     </div>
+    <script>
+        window.addEventListener('DOMContentLoaded', function () {
+            const chat001 = document.getElementById('chat-001');
+            const chat002 = document.getElementById('chat-002');
+            const chat003 = document.getElementById('chat-003');
+            const chat004 = document.getElementById('chat-004');
+            const inputChat = document.getElementById('chatBot-input');
+            chat001.addEventListener('click', function () {
+                inputChat.value = chat001.dataset.value;
+            });
+            chat002.addEventListener('click', function () {
+                inputChat.value = chat002.dataset.value;
+            });
+            chat003.addEventListener('click', function () {
+                inputChat.value = chat003.dataset.value;
+            });
+            chat004.addEventListener('click', function () {
+                inputChat.value = chat004.dataset.value;
+            });
+
+        });
+    </script>
 @endif
